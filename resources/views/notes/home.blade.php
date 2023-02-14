6c4248177fa09e6f0a6ad1af597100b2fe676724
Michel Tsilavo <micheltsilavodev@protonmail.com>
2023-02-14 13:28:10 -0300

    Add info on home page if no data in db

--- resources/views/notes/home.blade.php
+++ resources/views/notes/home.blade.php
@@ -4,16 +4,17 @@
    
     <div style="margin-top: 25px; ">
 
-        {{-- <ul style="list-style: none">
-            @foreach ($notes as $note)
-                <li>{{$note->note}} <a href="{{route('notes.show', $note)}}">Read more</a></li>
-            @endforeach
-        </ul> --}}
+      @if ($notes->count()>0)
+          
+      @foreach ($notes as $note)
+      <div style=" background-color:rgb(51, 41, 41); color: white; padding: 25px; margin: 10px 0; display: flex; flex-direction:column ">
+          {{$note->note}} <a href="{{route('notes.show', $note)}}">Read more</a>
+      </div>
+  @endforeach
+  @else
+  <h2 style="text-align:center; margin: 35% 0">Note is empty! Please add note</h2>   
+      @endif
+
         
-            @foreach ($notes as $note)
-            <div style=" background-color:rgb(51, 41, 41); color: white; padding: 25px; margin: 10px 0; display: flex; flex-direction:column ">
-                {{$note->note}} <a href="{{route('notes.show', $note)}}">Read more</a>
-            </div>
-        @endforeach
     </div>
 @endsection
